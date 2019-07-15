cc15800e88f2212d4038004a5b5c1d81b8077393
Yash Jain <yjain@clearblade.com>
2019-07-15 12:18:47 -0500

    Added configuration template

---
+++ configuration/base-template.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+    KEYS:{
+        TWILIO:"fkdshdafrchr389th34t82bnusfdnfdd==",
+        SENDGRID:"nsfuei3buw4ogsurb3w982bt34ioascjd=="
+    },
+
+    URL:{
+        TWILIO_BASE_URL: "api.twilio.com",
+        SENDGRID_BASE_URL: "api.sendgrid.com"
+    }
+}
